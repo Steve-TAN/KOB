1b7c182f77b7f22f9136687b9535a7b0f90a2e20
Steve-TAN <tttanzzyy@gmail.com>
2022-11-17 19:36:31 +0800

    登录模块

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PkIndexView from '@/views/pk/PkIndexView'
-import RecordIndexView from '@/views/record/RecordIndexView'
-import RanklistIndexView from '@/views/ranklist/RanklistIndexView'
-import UserBotIndexView from '@/views/user/bot/UserBotIndexView'
-import NotFound from '@/views/error/NotFound'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
+import NotFound from '../views/error/NotFound'
+import UserAccountLoginView from '../views/user/account/UserAccountLoginView'
+import UserAccountRegisterView from '../views/user/account/UserAccountRegisterView'
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
   {
     path: "/pk/",
     name: "pk_index",
@@ -15,33 +22,35 @@ const routes = [
     path: "/record/",
     name: "record_index",
     component: RecordIndexView,
-    meta: {
-      requestAuth: true,
-    }
   },
   {
     path: "/ranklist/",
     name: "ranklist_index",
     component: RanklistIndexView,
-    meta: {
-      requestAuth: true,
-    }
   },
   {
     path: "/user/bot/",
     name: "user_bot_index",
     component: UserBotIndexView,
-    meta: {
-      requestAuth: true,
-    }
+  },
+  {
+    path: "/user/account/login/",
+    name: "user_account_login",
+    component: UserAccountLoginView,
+  },
+  {
+    path: "/user/account/register/",
+    name: "user_account_register",
+    component: UserAccountRegisterView,
   },
   {
     path: "/404/",
     name: "404",
     component: NotFound,
-    meta: {
-      requestAuth: false,
-    }
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
   }
 ]
 
