7ae26457f35a4323997d5e215fa464923129e85a
Steve-TAN <tttanzzyy@gmail.com>
2022-12-05 20:36:15 +0800

    个人页面的bot增删改查

---
+++ backend/src/main/java/com/kob/backend/controller/user/bot/GetListController.java
@@ -0,0 +1,19 @@
+package com.kob.backend.controller.user.bot;
+import com.kob.backend.pojo.Bot;
+import com.kob.backend.service.user.bot.GetListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GetListController {
+  @Autowired
+  private GetListService getListService;
+
+  @PostMapping("/user/bot/getlist/")
+  public List<Bot> getList() {
+    return getListService.getList();
+  }
+}
--- backend/src/main/java/com/kob/backend/pojo/User.java
+++ backend/src/main/java/com/kob/backend/pojo/User.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author tzy
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
