0202acf708b1d5c7aa97ab6b5cc6545d4fe9de38
Steve-TAN <tttanzzyy@gmail.com>
2022-12-06 19:48:15 +0800

    我的bot 前端页面

--- backend/src/main/java/com/kob/backend/controller/user/bot/GetListController.java
+++ backend/src/main/java/com/kob/backend/controller/user/bot/GetListController.java
@@ -2,7 +2,7 @@ package com.kob.backend.controller.user.bot;
 import com.kob.backend.pojo.Bot;
 import com.kob.backend.service.user.bot.GetListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +12,7 @@ public class GetListController {
   @Autowired
   private GetListService getListService;
 
-  @PostMapping("/user/bot/getlist/")
+  @GetMapping("/user/bot/getlist/")
   public List<Bot> getList() {
     return getListService.getList();
   }
